b2023b2387920ddb170c549b8cbbc676d04238dc
akm <akm2000@gmail.com>
2020-02-21 12:48:30 +0900

    Ints.Averageを追加

--- 03-02-answer1/people.go
+++ 03-02-answer1/people.go
@@ -2,8 +2,8 @@ package main
 
 type People []*Person
 
-func (s People) Ages() []int {
-	r := make([]int, len(s))
+func (s People) Ages() Ints {
+	r := make(Ints, len(s))
 	for idx, i := range s {
 		r[idx] = i.Age()
 	}
