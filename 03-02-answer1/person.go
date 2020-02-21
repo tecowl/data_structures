92e2792f315732de275e6a9c9ea457de991725db
akm <akm2000@gmail.com>
2020-02-21 12:47:14 +0900

    Person.Ageを追加

--- 03-02-answer1/person.go
+++ 03-02-answer1/person.go
@@ -1,8 +1,21 @@
 package main
 
+import (
+	"time"
+)
+
 type Person struct {
 	FirstName string
 	LastName  string
 	BloodType BloodType
 	Birthday  string
+}
+
+func (x *Person) Age() int {
+	now := time.Now()
+	d, err := time.Parse("2006-01-02", x.Birthday)
+	if err != nil {
+		panic(err)
+	}
+	return now.Year() - d.Year()
 }
