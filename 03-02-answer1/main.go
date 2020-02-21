f2e3220cc486f49067dc7cb11b47e740071c0bc9
akm <akm2000@gmail.com>
2020-02-21 12:45:46 +0900

    averageを抽出

--- 03-02-answer1/main.go
+++ 03-02-answer1/main.go
@@ -2,45 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	now := time.Now()
-	ages := make([]int, len(people))
-	agesMap := map[BloodType][]int{
-		O:  []int{},
-		A:  []int{},
-		B:  []int{},
-		AB: []int{},
-	}
-	for idx, person := range people {
-		d, err := time.Parse("2006-01-02", person.Birthday)
-		if err != nil {
-			panic(err)
-		}
-		age := now.Year() - d.Year()
-		ages[idx] = age
-		agesMap[person.BloodType] = append(agesMap[person.BloodType], age)
+	var people = []*Person{
+		{"山田", "太郎", O, "1960-01-02"},  // 60
+		{"鈴木", "一郎", A, "1970-03-04"},  // 50
+		{"佐藤", "真一", A, "1980-05-06"},  // 40
+		{"加藤", "一茶", B, "1990-07-08"},  // 30
+		{"伊藤", "浩史", AB, "2000-09-10"}, // 20
 	}
 
-	sum := 0
-	for _, age := range ages {
-		sum += age
-	}
+	allAvg, avgMap := averages(people)
 
-	sumMap := map[BloodType]int{}
-	for bloodType, ages := range agesMap {
-		s := 0
-		for _, age := range ages {
-			s += age
-		}
-		sumMap[bloodType] = s
-	}
-
-	fmt.Printf("全体平均年齢: %.02f 歳\n", float32(sum)/float32(len(people)))
-	fmt.Printf("O型平均年齢: %.02f 歳\n", float32(sumMap[O])/float32(len(agesMap[O])))
-	fmt.Printf("A型平均年齢: %.02f 歳\n", float32(sumMap[A])/float32(len(agesMap[A])))
-	fmt.Printf("B型平均年齢: %.02f 歳\n", float32(sumMap[B])/float32(len(agesMap[B])))
-	fmt.Printf("AB型平均年齢: %.02f 歳\n", float32(sumMap[AB])/float32(len(agesMap[AB])))
+	fmt.Printf("全体平均年齢: %.02f 歳\n", allAvg)
+	fmt.Printf("O型平均年齢: %.02f 歳\n", avgMap[O])
+	fmt.Printf("A型平均年齢: %.02f 歳\n", avgMap[A])
+	fmt.Printf("B型平均年齢: %.02f 歳\n", avgMap[B])
+	fmt.Printf("AB型平均年齢: %.02f 歳\n", avgMap[AB])
 }
